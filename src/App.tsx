1e680664d4f023ea256a7628ce3bcb2e8d1533cd
adewoleoluwajuwon <adewoleoluwajuwon@gmail.com>
2025-10-03 15:54:10 +0100

    Auth added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AppShell from "./components/AppShell";
 import Landing from "./pages/Landing";
 import Jobs from "./pages/Jobs";
 import JobDetails from "./pages/JobDetails";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="jobs" element={<Jobs />} />
         <Route path="/jobs/:id" element={<JobDetails />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </AppShell>
   );
---
+++ src/pages/Auth.tsx
@@ -0,0 +1,5 @@
+import AuthCard from "../components/AuthCard";
+
+export default function Auth() {
+  return <AuthCard />;
+}
